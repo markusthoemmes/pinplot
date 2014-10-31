5a41a7788d39a7fdb51b30a433b86346b64eb11e
Markus Thömmes <markusthoemmes@me.com>
2014-10-31 23:18:11 +0000

    Basic API running, sample data included

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-storymapper
+StoryMapper
 ===========
+
+## Backend
+1. Make sure Node.js and MongoDB are installed.
+2. Go to "backend" and run `npm install`.
+3. Run `npm start`.
---
+++ backend/index.js
@@ -0,0 +1,38 @@
+var restify = require('restify');
+var mongojs = require('mongojs');
+
+var server = restify.createServer({
+	name: 'StoryMapper',
+});
+
+server
+	.use(restify.fullResponse())
+	.use(restify.bodyParser());
+
+server.get('/book/:id', function(req, res, next) {
+	res.contentType = 'json';
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+	var db = mongojs('storymapper', ['books']);
+
+	if(req.params.id === '') {
+		db.books.find(function(err, docs) {
+	    	res.send({books: docs});
+		});
+	}
+	else {
+		db.books.findOne({_id: mongojs.ObjectId(req.params.id)},function(err, docs) {
+	    	res.send({books: docs});
+		});
+	}
+	return next();
+});
+
+server.post('/book', function(req, res, next) {
+	console.log(req.params);
+	res.send("done");
+	return next();
+});
+
+server.listen(8080);
