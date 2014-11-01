bbcc36bff0b22b5fc57b471396f0eab4a90152e1
achim <kramer>
2014-11-01 13:28:06 +0100

    Integrated new code from markus JS

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -6,59 +6,57 @@ var app = {};
 app.run = function(settingsObj) {
     
     // confuguration
-    appData.apiURL = settingsObj['appData']['apiURL'];
+    appData.apiURL = settingsObj.appData.apiURL;
 
-    appData.getBook('54540dcce3118013501512b0', function(data) {
+    appData.getBook('5454cc8ce3118013501512b1', function(data) {
+
+    	var characters = _.map(data.events, function(event) {
+    		return _.pluck(event.involved, 'name');
+    	});
+    	characters = _.unique(_.flatten(characters));
 
         appMap.createMap("map", {
             center: new google.maps.LatLng(data.events[0].location.lat, data.events[0].location.lon)
         });
 
-        var createObjectsInBounds = function(map, lowerBound, upperBound) {
-            var coordinates = [];
-            for(var i = 0; i < data.events.length; i++) {
-        var currentEvent = data.events[i];
-        if (currentEvent.characterCount >= lowerBound && currentEvent.characterCount <= upperBound) {
+        var createObjects = function(map, characters, lowerBound, upperBound) {
+	        _.each(characters, function(character) {
+	        	var events = _.filter(data.events, function(event) {
+	        		return _.contains(_.pluck(event.involved, 'name'), character) && event.characterCount >= lowerBound && event.characterCount <= upperBound;
+	        	});
 
-            appMap.addObject(new google.maps.Marker({
-                position: new google.maps.LatLng(currentEvent.location.lat, currentEvent.location.lon)
-            }));
+	        	var coordinates = [];
+	        	_.each(events, function(event) {
+	        		appMap.addObject(new google.maps.Marker({
+		                position: new google.maps.LatLng(event.location.lat, event.location.lon)
+		            }));
 
-            coordinates.push(new google.maps.LatLng(currentEvent.location.lat, currentEvent.location.lon));
-        }
-    }
+		            coordinates.push(new google.maps.LatLng(event.location.lat, event.location.lon));
+	        	});
 
-    appMap.addObject(new google.maps.Polyline({
-            path: coordinates,
-            strokeColor: "#FF0000",
-            strokeOpacity: 1.0,
-            strokeWeight: 2
-        }));
-    };
-    createObjectsInBounds(map, data.events[0].characterCount, data.events[data.events.length - 1].characterCount);
+	        	appMap.addObject(new google.maps.Polyline({
+		            path: coordinates,
+		            strokeColor: "#FF0000",
+		            strokeOpacity: 1.0,
+		            strokeWeight: 2,
+		            geodesic: true
+		        }));
+	        });
+	    };
+
+	    createObjects(map, ["Sabeth"], data.events[0].characterCount, data.events[data.events.length - 1].characterCount);
 
 
-    $("#pageline-slider").rangeSlider({
-        step: 1,
-        bounds: {min: data.events[0].characterCount, max: data.events[data.events.length - 1].characterCount},
-        defaultValues: {min: data.events[0].characterCount, max: data.events[data.events.length - 1].characterCount}
-    });
-    $('#pageline-slider').bind('valuesChanging', function (e, data) {
-        appMap.clearMap();
-        createObjectsInBounds(map, data.values.min, data.values.max);
-    });
-    
-    
-    // Initialize jQuery slider
-//            $("#timeline-sider").dateRangeSlider();
-//
-//            // @TODO: use data 
-//            $("#timeline-slider").dateRangeSlider("option", "bounds", {
-//                min: new Date(2012, 0, 1),
-//                max: new Date(2012, 11, 31)
-//            });
-    
-});
+	    $("#pageline-slider").rangeSlider({
+	        step: 1,
+	        bounds: {min: data.events[0].characterCount, max: data.events[data.events.length - 1].characterCount},
+	        defaultValues: {min: data.events[0].characterCount, max: data.events[data.events.length - 1].characterCount}
+	    });
+	    $('#pageline-slider').bind('valuesChanging', function (e, data) {
+	        appMap.clearMap();
+	        createObjects(map, ["Sabeth"], data.values.min, data.values.max);
+	    });
+	});
     
 };
 
