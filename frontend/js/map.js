7b5c5397ea2ee5e75bb9540b959d381a9e9143f8
achim <kramer>
2014-11-01 11:50:32 +0100

    Integrated data- and map-JS

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -2,6 +2,68 @@ var appMap = {};
 appMap.map = null;
 appMap.objects = [];
 
+appMap.createMap = function(id) {
+	var styles = [
+	  {
+	    "featureType": "landscape.natural",
+	    "stylers": [
+	      { "visibility": "on" },
+	      { "color": "#808080" },
+	      { "lightness": -26 },
+	      { "gamma": 0.66 }
+	    ]
+	  },{
+	    "featureType": "road",
+	    "stylers": [
+	      { "visibility": "on" },
+	      { "saturation": -79 },
+	      { "lightness": -34 },
+	      { "hue": "#005eff" }
+	    ]
+	  },{
+	    "featureType": "water",
+	    "stylers": [
+	      { "lightness": -51 },
+	      { "gamma": 0.38 },
+	      { "saturation": -88 }
+	    ]
+	  },{
+	    "featureType": "landscape.man_made",
+	    "elementType": "geometry",
+	    "stylers": [
+	      { "color": "#fbef69" }
+	    ]
+	  },{
+	    "elementType": "labels.text.stroke",
+	    "stylers": [
+	      { "lightness": 17 },
+	      { "color": "#fbef69" },
+	      { "saturation": -37 }
+	    ]
+	  },{
+	    "featureType": "poi",
+	    "stylers": [
+	      { "weight": 0.6 },
+	      { "color": "#808080" },
+	      { "visibility": "off" }
+	    ]
+	  },{
+	    "featureType": "administrative.land_parcel"  }
+	];
+
+	var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"});
+
+	var mapOptions = {
+		zoom: 8,
+		mapTypeControlOptions: {
+    		mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    	}
+	};
+	appMap.map = new google.maps.Map(document.getElementById(id), mapOptions);
+	appMap.map.mapTypes.set('map_style', styledMap);
+    appMap.map.setMapTypeId('map_style');
+};
+
 appMap.addObject = function(marker) {
 	marker.setMap(appMap.map);
 	appMap.objects.push(marker);
